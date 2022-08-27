2ed4d8ea798f7cfacd9ca368cdedd398e7bcf64e
Mustafa Selim Gunaydin <mustafaselimgunaydin@gmail.com>
2022-08-27 13:48:39 +0300

    spaces deleted

--- HepsiburadaUITestAutomation/src/test/java/TestRunner.java
+++ HepsiburadaUITestAutomation/src/test/java/TestRunner.java
@@ -1,24 +1,14 @@
-
 import org.junit.runner.RunWith;
-
-
 import io.cucumber.junit.Cucumber;
-
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(features = ".\\src\\test\\resources\\features\\CaseStudy.feature",
-
         glue = {"steps"},
-
         plugin = {"pretty", "json:target/cucumber-reports/reports.json",
-
                 "junit:target/cucumber-reports/Cucumber.xml",
-
                 "html:target/cucumber-reports/reports2.html"},
         monochrome = true,
-
         dryRun = false)
 
 public class TestRunner {
