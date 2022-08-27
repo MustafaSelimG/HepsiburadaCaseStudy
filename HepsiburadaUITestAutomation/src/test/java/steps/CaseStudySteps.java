43b87a6bb2ca462b5e8695065dccdecddd917edc
Mustafa Selim Gunaydin <mustafaselimgunaydin@gmail.com>
2022-08-27 12:18:32 +0300

    steps updated

--- HepsiburadaUITestAutomation/src/test/java/steps/CaseStudySteps.java
+++ HepsiburadaUITestAutomation/src/test/java/steps/CaseStudySteps.java
@@ -4,29 +4,38 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.Homepage;
+import pages.ProductPage;
+import pages.SearchResultPage;
 
 public class CaseStudySteps {
+    Homepage homepage = new Homepage();
+    SearchResultPage searchResultPage = new SearchResultPage();
+    ProductPage productPage = new ProductPage();
+
     @Given("verify that homepage is open")
     public void verifyThatHomepageIsOpen() {
+        homepage.verifyHomepageSlider();
     }
 
     @When("I search for {string}")
     public void ıSearchFor(String searchKey) {
+        homepage.searchFor(searchKey);
     }
 
-    @And("I click on product number {int} in the search result page")
-    public void ıClickOnProductNumberInTheSearchResultPage(int productNumber) {
+    @And("I click on product number {string} in the search result page")
+    public void ıClickOnProductNumberInTheSearchResultPage(String productNumber) {
+        searchResultPage.clickForProductNumber(productNumber);
     }
 
     @And("I navigate to the comments tab")
     public void ıNavigateToTheCommentsTab() {
+        productPage.openReviewsTab();
     }
 
-    @And("I give plus point to the comment number {int}")
-    public void ıGivePlusPointToTheCommentNumber(int commentNumber) {
+    @Then("I give plus point to the comment number {string} and assert if a comment exist")
+    public void ıGivePlusPointToTheCommentNumberAndAssertIfACommentExist(String commentNumber) {
+        productPage.givePlusPointToTheReviewNumber(commentNumber);
     }
 
-    @Then("assert that title is seen")
-    public void assertThatTitleIsSeen() {
-    }
 }
