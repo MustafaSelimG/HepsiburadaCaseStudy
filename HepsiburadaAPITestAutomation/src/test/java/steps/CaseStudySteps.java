7b9213390a27c55112ecc7fe8f8ec939cc08c53b
Mustafa Selim Gunaydin <mustafaselimgunaydin@gmail.com>
2022-08-28 08:42:04 +0300

    Update CaseStudySteps.java

--- HepsiburadaAPITestAutomation/src/test/java/steps/CaseStudySteps.java
+++ HepsiburadaAPITestAutomation/src/test/java/steps/CaseStudySteps.java
@@ -1,22 +1,36 @@
 package steps;
 
+import helpers.HttpHelper;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.Assert;
+
+import java.io.File;
 
 public class CaseStudySteps {
-    String URI = "/gen/clients";
+    String basePath = "/gen/clients";
     Response response;
 
     @Given("send get request with language for clients library")
     public void sendGetRequestWithLanguageForClientsLibrary() {
-    }
+        String path = basePath + "/java";
 
-    @And("assert status code")
-    public void assertStatusCode() {
+        response = HttpHelper.getRequest(path);
     }
 
     @Given("send post request with language for clients library")
     public void sendPostRequestWithLanguageForClientsLibrary() {
+        String path = basePath + "/java";
+
+        File jsonData = new File("src/main/java/jsonFiles/java.json");
+
+        response = HttpHelper.postRequestFile(path, jsonData);
+    }
+
+    @And("assert status code")
+    public void assertStatusCode() {
+        Assert.assertEquals(HttpStatus.SC_OK, response.statusCode());
     }
 }
